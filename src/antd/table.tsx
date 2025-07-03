4e370718e9ebb39210c5333f68be0574ac4e8219
bens <5878794@qq.com>
2025-07-03 15:10:46 +0800

    add

--- src/antd/table.tsx
+++ src/antd/table.tsx
@@ -3,10 +3,13 @@
 import {ReactComponent,withNavigation} from "@/lib/defineComponent";
 import {Table} from "antd";
 import device from "@/lib/device.ts";
+import React from 'react'
 
-//TODO 滚动 合计 输入控件 表格合并 样式控制
+//TODO 输入控件 表格合并 样式控制
 
 class myTable extends ReactComponent{
+    tableRef = React.createRef(); // 创建一个 ref
+
     constructor(props:any) {
         super(props);
 
@@ -15,6 +18,7 @@ class myTable extends ReactComponent{
             dataSource:this.handlerData(),
             columns:this.handlerSetting(),
             selected:this.props.selected,
+            scrollYMaxHeight:500
         }
     }
 
@@ -24,6 +28,8 @@ class myTable extends ReactComponent{
         //参数说明   sort:数字列的排序
         //参数说明   customSort:自定义排序 (a,b)=>a.a-b.a   //a、b为行的数据
         //参数说明   format: type=date时显示时间格式化的参数   默认 YYYY-MM-DD   完整格式 YYYY-MM-DD hh:mm:ss
+        //参数说明   width:列的宽度  单位px
+        //参数说明   fixed:滚动时固定不动  left | right
         //参数说明   type:显示类型
             // text：文本
             // date:时间戳显示成日期
@@ -47,7 +53,30 @@ class myTable extends ReactComponent{
         selectType:null,     //  null  | checkbox | radio  如果为null下面2个参数无效
         onSelect:null,  //  null  |  (selectedRowkeys)=>{...}
         selected:[],     //当前选中的行的key
+        totalKeys:[]    //需要合计的字段key  第一列默认写成合计，如果要统计第一列setting中要加一个空列
+    }
 
+    ready(){
+        this.autoSetTableMaxHeight();
+    }
+
+    //自动设定table的最大高度，滚动需要
+    autoSetTableMaxHeight(){
+        const dom = (this.tableRef.current as any).children[0];
+        const run = () => {
+            const header = dom.getElementsByClassName('ant-table-header')[0];
+            const summer = dom.getElementsByClassName('ant-table-summary')[0];
+
+            this.setState({
+                scrollYMaxHeight:dom.parentElement.offsetHeight-(header?.offsetHeight??0)-(summer?.offsetHeight??0)
+            })
+        }
+
+        run();
+
+        window.onresize = () => {
+            run();
+        }
     }
 
     //处理表格配置
@@ -107,6 +136,13 @@ class myTable extends ReactComponent{
                 thisObj.sorter = rs.customSort;
             }
 
+            if(rs.width){
+                thisObj.width = rs.width
+            }
+
+            if(rs.fixed){
+                thisObj.fixed = rs.fixed
+            }
 
 
             newSetting.push(thisObj);
@@ -126,6 +162,41 @@ class myTable extends ReactComponent{
         return newData;
     }
 
+    //计算合计
+    renderTotal(data:any){
+        const total:any = {};
+        this.props.totalKeys.map((rs:any)=>{
+            total[rs] = 0;
+        })
+
+        data.map((rs:any)=>{
+            this.props.totalKeys.map((key:string)=>{
+                total[key] = total[key] + (rs[key]??0)
+            })
+        })
+
+
+        if(this.props.totalKeys && this.props.totalKeys.length > 0){
+            return (
+                <Table.Summary fixed>
+                    <Table.Summary.Row>
+                        {this.props.selectType && <Table.Summary.Cell key='__total__' index={0}></Table.Summary.Cell>}
+                        {this.props.setting.map((rs:any,i:number)=>{
+                            if(i==0){
+                                return <Table.Summary.Cell key={'__total__'+i}  index={i}>合计</Table.Summary.Cell>
+                            }else{
+                                return <Table.Summary.Cell key={'__total__'+i} index={i}>{total[rs.key]}</Table.Summary.Cell>
+                            }
+                        })}
+                    </Table.Summary.Row>
+                </Table.Summary>
+            );
+
+        }else{
+            return null
+        }
+    }
+
 
     render(){
         const rowSelection:any = !this.props.selectType? null : {
@@ -142,12 +213,21 @@ class myTable extends ReactComponent{
             }
         }
 
+
         return <Table
-            className='w_100 h_100'
+            ref={this.tableRef as any}
+            className={'h_100 w_100'}
             dataSource={this.state.dataSource}
             columns={this.state.columns}
             rowSelection={rowSelection}
             pagination={false}
+            // bordered
+            // size="small"
+            summary={(data:any)=>{return this.renderTotal(data)}}
+            scroll={{
+                x:'max-content',
+                y:this.state.scrollYMaxHeight+'px'
+            }}
         />
     }
 
--- src/pages/abc/tt/1.tsx
+++ src/pages/abc/tt/1.tsx
@@ -9,17 +9,17 @@ class Page extends ReactComponent{
         super(props);
         this.state = {
             setting:[
-                {label:'列1',type:'text',key:'a',sort:true},
-                {label:'列444',type:'text',key:'aa',customSort:(a:any,b:any)=>{
+                {label:'列1',type:'text',key:'a',sort:true,width:100,fixed: 'left'},
+                {label:'列444',width:2000,type:'text',key:'aa',customSort:(a:any,b:any)=>{
                     return (a.aa??0)-(b.aa??0)
                 }},
-                {label:'列1',type:'date',key:'b',format:'YYYY-MM-DD'},
-                {label:'列2',type:'custom',render:(rowData:any)=>{
+                {label:'列1',width:150,type:'date',key:'b',format:'YYYY-MM-DD'},
+                {label:'列2',type:'custom',width:100,fixed:'right',render:(rowData:any)=>{
                     return <a>{rowData.c}</a>
                 }},
             ],
             data:[  //最好带id字段，应为每行数据要有自己的key，否则会自动生成 row_i 的key
-                {id:1,a:1,aa:33,b:new Date().getTime(),c:3},
+                {id:1,a:'阿萨地方噶开始两地分居阿迪斯李逵负荆阿斯利康电极法',aa:33,b:new Date().getTime(),c:3},
                 {id:2,a:2,aa:55,b:Math.floor(new Date().getTime()/1000),c:3},
                 {id:3,a:5,aa:64,b:2,c:3},
                 {id:4,a:11,aa:3,b:2,c:3},
@@ -43,7 +43,8 @@ class Page extends ReactComponent{
                     onSelect={(rs:any)=>{
                         console.log(rs)
                     }}
-                    selectType='checkbox'
+                    // selectType='checkbox'
+                    totalKeys={['aa']}
                 />
             </div>
 
